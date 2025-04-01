80a2d8e63773ac86444b4a4036eff11831d72e09
Isabella Hunter <isabella.hunter2001@gmail.com>
2025-04-01 11:50:23 +0100

    css updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
-function Hero(){
-    return ( <section>
-        <h2>Welcome to NC News </h2>
-        <break/>
-       <h3> Where News Meets Community.</h3>
-       <break/>
-       <img className="hero-image" src="src/assets/filip-mishevski-c5QdMcuFlgY-unsplash.jpg" alt="man reading a news wall" />
-    </section>)
+function Hero() {
+  return (
+    <section>
+      <h2>Welcome to NC News </h2>
+      <br />
+      <h3> Where News Meets Community.</h3>
+      <br />
+      <img
+        className="hero-image"
+        src="src/assets/filip-mishevski-c5QdMcuFlgY-unsplash.jpg"
+        alt="man reading a news wall"
+      />
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
