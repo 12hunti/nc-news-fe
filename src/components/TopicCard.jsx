422bd30f47edf2fade3cfd360532f5f179579f12
Isabella Hunter <isabella.hunter2001@gmail.com>
2025-04-03 14:10:32 +0100

    styling changes

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,7 +1,7 @@
 function TopicCard({ topic }) {
   return (
     <li>
-      <img src={topic.img_url} alt={topic.slug} />
+      <img className="topic-image" src={topic.img_url || "src/assets/filip-mishevski-c5QdMcuFlgY-unsplash.jpg"} alt={topic.slug} />
       <h3>{topic.slug}</h3>
       <p>{topic.description}</p>
     </li>
